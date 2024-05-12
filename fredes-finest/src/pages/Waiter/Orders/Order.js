6bf4ba03279f9be7142e03e23fe832558a648528
ramsus1311 <rasmus1311@live.dk>
2024-05-13 00:54:46 +0200

    Fixed Order.js import

--- fredes-finest/src/pages/Waiter/Orders/Order.js
+++ fredes-finest/src/pages/Waiter/Orders/Order.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import { useOrder } from '../../../services/OrderContext';
+import Order from '../../../Components/OrderItem/Order';
 
 function OrderView() {
-  const { currentOrder } = useOrder(); // Access currentOrder state
+  const { currentOrder } = useOrder();
 
   return (
     <div>
       <h1>Orders</h1>
-      {currentOrder.map((orderItem, index) => (
-        <div key={index}>
-          {/* Render order items here */}
-          <p>Order Item: {orderItem.menuItem.name} - Quantity: {orderItem.quantity}</p>
-        </div>
-      ))}
+      <Order/>
     </div>
   );
 }
