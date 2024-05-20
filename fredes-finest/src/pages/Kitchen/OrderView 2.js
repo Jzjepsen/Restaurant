a0d4ba7bebf744c0a081b255bca6ee09614b0814
Jzjepsen <jzjepsen@gmail.com>
2024-05-20 23:09:34 +0200

    added to main

---
+++ fredes-finest/src/pages/Kitchen/OrderView 2.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Order from '../../Components/OrderItem/Order';
+import './OrderView.css'; // Import the CSS file
+import { useOrder } from '../../services/OrderContext';
+
+
+function KitchenOrderView() {
+    const { orderItems, updateOrderStatus } = useOrder();
+  
+    return (
+      <div className="order-view-container">
+        <h1>Kitchen Order View</h1>
+        <div className="order-column-container">
+          <div className="order-column">
+            <h2>New Order</h2>
+            {orderItems.filter(item => item.status === 'new order').map(item => (
+                <Order key={item.id} status={item.status} menuItem={item.menuItem} quantity={item.quantity} comment={item.comment} updateStatus={updateOrderStatus} id={item.id} isKitchenView={true} />
+            ))}
+
+          </div>
+          <div className="order-column">
+            <h2>Preparing</h2>
+            {orderItems.filter(item => item.status === 'preparing').map(item => (
+            <Order key={item.id} status={item.status} menuItem={item.menuItem} quantity={item.quantity} comment={item.comment} updateStatus={updateOrderStatus} id={item.id} isKitchenView={true} />
+        ))}
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+export default KitchenOrderView;
